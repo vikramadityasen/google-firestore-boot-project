6961cf2cf42966a46fc0ea2d9827abf60b57012a
Vikram Aditya Sen <sen.vikramaditya111@gmail.com>
2020-07-19 12:20:36 +0530

    initial commit
    First commit

---
+++ src/main/java/com/dgtl/practice/firebase/FirebaseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dgtl.practice.firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgtl/practice/firebase/FirebaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgtl.practice.firebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
